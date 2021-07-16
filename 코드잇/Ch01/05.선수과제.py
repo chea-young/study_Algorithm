a47005e1082dfcf7f1db28da81c7ed3e09f6699f
chea-young <63410509+chea-young@users.noreply.github.com>
2021-07-16 19:01:16 +0900

    feat: add ch01-1

--- 코드잇/Ch01/05.선수과제.py
+++ 코드잇/Ch01/05.선수과제.py
@@ -1,13 +1,12 @@
-"""
-"토마토"나 "기러기"처럼 거꾸로 읽어도 똑같은 단어를 팔린드롬(palindrome)이라고 부릅니다. 
-문자열 word가 팔린드롬인지 확인하는 함수 is_palindrome를 쓰세요. 
-is_palindrome은 word가 팔린드롬이면 True를, 팔린드롬이 아니면 False를 리턴합니다.
-"""
-
 def is_palindrome(word):
-    # 코드를 입력하세요.
+    count_num = len(word)//2
+    for i in range(count_num):
+        last_num = i*(-1) -1
+        if(word[i] != word[last_num]):
+            return False
+    return True
 
-# 테스트
+# TEST
 print(is_palindrome("racecar"))
 print(is_palindrome("stars"))
 print(is_palindrome("토마토"))
