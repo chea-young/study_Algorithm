4c272eae3201282f31921cf563ac53038ea19f09
chea-young <63410509+chea-young@users.noreply.github.com>
2021-08-04 20:26:52 +0900

    fix: fix error 01-Q4

--- CODETREE/1일차 - Basic/01- Q4 보고 말하는 수열.py
+++ CODETREE/1일차 - Basic/01- Q4 보고 말하는 수열.py
@@ -1,26 +1,21 @@
-#FIX 연속한 수로 바꾸기
-def make_word(num_dic):
-    ans = ''
-    num_list = sorted(list(num_dic.keys()))
-    for i in num_list:
-        ans += str(i)
-        ans += str(num_dic[i])
-    return ans
-
 str_num = '1'
 ans = ''
 count_num = int(input())
-num_dic = {}
 for i in range(count_num-1):
+    temp_str = ''
     if count_num == 1:
         break
-    num_dic = {}
-    for num in str_num:
-        if num in num_dic.keys():
-            count = num_dic[num]
-            num_dic[num] = count +1
+    check = str_num[0]
+    count = 0
+    for i in str_num:
+        if check == i:
+            count +=1
         else:
-           num_dic[num] = 1 
-    str_num = make_word(num_dic)
+            temp_str += check
+            temp_str += str(count)
+            check = i
+            count = 1
+    temp_str += check
+    temp_str += str(count)
+    str_num = temp_str
 print(str_num)
-
